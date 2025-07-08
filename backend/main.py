69bf6d5341ba6600863ef289e79eb4d8b95657be
Eric Doering <ericbenjamindoering@gmail.com>
2025-07-08 15:40:05 -0400

    endpoint created for GET contacts

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,17 @@ from flask import request, jsonify
 from config import app, db
 from models import Contact
 
+# GET
+@app.route('/contacts', methods=['GET'])
+def get_contacts():
+    contacts = Contact.query.all()
+    json_contacts = list(map(lambda x: x.to_json(), contacts))
+    return jsonify({
+        'contacts': json_contacts
+    })
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
+
     app.run(debug=True)
