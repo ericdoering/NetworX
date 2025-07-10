59b98cf50304ee82821a673bec81cf4b80fa3a22
Eric Doering <ericbenjamindoering@gmail.com>
2025-07-10 15:43:42 -0400

    create context route created

--- backend/main.py
+++ backend/main.py
@@ -11,6 +11,30 @@ def get_contacts():
         'contacts': json_contacts
     })
 
+@app.route('create_context', methods=['POST'])
+def create_contact():
+    first_name = request.json.get('firstName')
+    last_name = request.json.get('lastName')
+    email = request.json.get('email')
+
+    if not first_name or not last_name or not email:
+        return (
+            jsonify({
+            'message': 'You must include a first name, last name, or email.'}),
+            400,
+        )
+    new_contact = Contact(first_name, last_name, email)
+    try:
+        db.session.add(new_contact)
+        db.session.commit()
+    except Exception as e:
+        return (
+            jsonify({
+            'message': str(e)}),
+            400,
+        )
+    return jsonify({'message': 'New User Created!'}), 201
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
