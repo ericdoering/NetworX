fb376f7da8e2a1741f4099f4f3d4e24bf035406e
Eric Doering <ericbenjamindoering@gmail.com>
2025-07-14 10:50:20 -0400

    delete contact endpoint added, frontend integrated with backend

--- backend/main.py
+++ backend/main.py
@@ -52,6 +52,14 @@ def update_contact(user_id):
 
     return jsonify({'message': 'User updated!'}), 200
 
+@app.route('/delete_contact/<int:user_id>', methods=['DELETE'])
+def delete_contact(user_id):
+    contact = Contact.query.get(user_id)
+
+    db.session.delete(contact)
+    db.session.commit({'message': 'User deleted!'}), 200
+
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
