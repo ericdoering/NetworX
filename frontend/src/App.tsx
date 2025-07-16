9cf9bd577f3801738e1317c264dd8fdb1bc52033
Eric Doering <ericbenjamindoering@gmail.com>
2025-07-15 21:53:53 -0400

    contact list created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import ContactList from './ContactList'
 import './App.css'
 
 function App() {
@@ -17,7 +18,7 @@ function App() {
 
   return (
     <>
-     
+      <ContactList contacts={contacts} />
     </>
   )
 }
---
+++ frontend/src/ContactList.tsx
@@ -0,0 +1,43 @@
+// import { useState } from 'react';
+
+interface Contact {
+    id: string | number;
+    firstName: string;
+    lastName: string;
+    email: string;
+}
+
+function ContactList({ contacts }: { contacts: Contact[] }) {
+  return (
+    <>
+        <div>
+            <h2>Contacts</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {contacts.map((contact: Contact) => (
+                        <tr key={contact.id}>
+                            <td>{contact.firstName}</td>
+                            <td>{contact.lastName}</td>
+                            <td>{contact.email}</td>
+                            <td>
+                                <button>Update</button>
+                                <button>Delete</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    </>
+  );
+}
+
+export default ContactList;
